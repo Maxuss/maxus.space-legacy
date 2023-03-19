ab304fa7a18ba6a70fba2d822733281ac1693d68
maxus <maxus.info.mail@gmail.com>
2023-03-19 20:54:00 +0300

    readme :weary:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>maxus lives here</title>
         <meta name="description" content="yea" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
